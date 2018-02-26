e6b8f987c17488efb8c0c37224688b739e94dbbc
Chris <chris@landlordwebsolutions.com>
2018-02-26 15:05:28 -0500

    Config::load() - prefix arg  - add the prefix followed by a dot to all config options loaded from the given file

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -11,20 +11,33 @@
 
         /**
          * @param string $filename
+         * @param string $prefix [optional]
+         *                       If prefix is used, all config options loaded will be indexed
+         *                       in dot notation ie prefix.option
          */
-        public static function load($filename) {
+        public static function load($filename, $prefix = '') {
             $reader = new ArrayFileReader($filename);
-            self::$config = merge(self::$config, $reader->loadArray());
+            $new_config = $reader->loadArray();
+
+            if (!empty($prefix)) {
+                foreach ($new_config as $key => $value) {
+                    $new_config[ $prefix . '.' . $key ] = $value;
+                    unset($new_config[ $key ]);
+                }
+            }
+
+            self::$config = merge(self::$config, $new_config);
         }
 
         /**
          * Get the value of a config var
          *
          * @param string $var_name
+         * @param mixed  $default
          *
          * @return mixed|null
          */
-        public static function val($var_name) {
-            return array_get(self::$config, strtolower($var_name));
+        public static function val($var_name, $default = null) {
+            return array_get(self::$config, strtolower($var_name), $default);
         }
     }
