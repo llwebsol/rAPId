b3afd41ca78674b0508707a1935cc85fd5a58705
Chris <chris@landlordwebsolutions.com>
2018-02-26 15:55:39 -0500

    Added DB helper and database_listeners config

--- require/database_listeners.php
+++ require/database_listeners.php
@@ -3,27 +3,29 @@
     use EasyDb\Events\Event;
 
     return [
-        /**
-         * Add a the fully qualified name
-         * of a class which implements EasyDb\Events\Listener
-         * to any of the following arrays in order to subscribe
-         * to the associated event
-         */
-        Event::ON_ERROR => [],
+        'listeners' => [
+            /**
+             * Add a the fully qualified name
+             * of a class which implements EasyDb\Events\Listener
+             * to any of the following arrays in order to subscribe
+             * to the associated event
+             */
+            Event::ON_ERROR => [],
 
-        Event::BEFORE_QUERY => [],
-        Event::AFTER_QUERY  => [],
+            Event::BEFORE_QUERY => [],
+            Event::AFTER_QUERY  => [],
 
-        // Update and Insert
-        Event::BEFORE_SAVE  => [],
-        Event::AFTER_SAVE   => [],
+            // Update and Insert
+            Event::BEFORE_SAVE  => [],
+            Event::AFTER_SAVE   => [],
 
-        Event::BEFORE_UPDATE => [],
-        Event::AFTER_UPDATE  => [],
+            Event::BEFORE_UPDATE => [],
+            Event::AFTER_UPDATE  => [],
 
-        Event::BEFORE_INSERT => [],
-        Event::AFTER_INSERT  => [],
+            Event::BEFORE_INSERT => [],
+            Event::AFTER_INSERT  => [],
 
-        Event::BEFORE_DELETE => [],
-        Event::AFTER_DELETE  => []
+            Event::BEFORE_DELETE => [],
+            Event::AFTER_DELETE  => []
+        ]
     ];
---
+++ src/Data/DB.php
@@ -0,0 +1,41 @@
+<?php
+
+    namespace rAPId\Data;
+
+    use EasyDb\Core\Config as EasyDbConfig;
+    use EasyDb\Core\ConnectionPool;
+    use EasyDb\Core\DbInstance;
+    use EasyDb\Events\Listeners;
+    use rAPId\Config\Config;
+
+    Config::load('config/database.php', 'db');
+    Config::load('config/database_listeners', 'db');
+    DB::registerListeners();
+
+    class DB
+    {
+
+        /**
+         * @throws \EasyDb\Exceptions\RegisterNonListenerException
+         */
+        public static function registerListeners() {
+            $listeners = Config::val('db.listeners');
+            foreach ($listeners as $event => $classes) {
+                foreach ($classes as $listener) {
+                    Listeners::register($event, $listener);
+                }
+            }
+        }
+
+        /**
+         * @param string $key
+         *
+         * @return DbInstance
+         */
+        public static function getDB($key = 'main') {
+            $config = Config::val("db.$key");
+            $easy_db_config = new EasyDbConfig($config[ $key ]);
+
+            return ConnectionPool::getDbInstance($easy_db_config);
+        }
+    }
