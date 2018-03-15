b6f08609781553c9010b2cd8b03168973db96ef3
Chris <chris@landlordwebsolutions.com>
2018-03-15 14:08:44 -0400

    XML Serializer : Modified order of member functions for readability

--- src/Data/Serialization/XmlSerializer.php
+++ src/Data/Serialization/XmlSerializer.php
@@ -65,6 +65,78 @@
         }
 
         /**
+         * @param mixed       $data
+         * @param string|null $parent_node
+         * @param string      $attributes [optional]
+         *
+         * @return string
+         */
+        private static function buildXml($data, $parent_node, $attributes = null) {
+            if (!is_array($data)) {
+                return self::wrapNode($parent_node, $data, $attributes);
+            }
+
+            $result = '';
+            $attributes = self::getAttributesString(array_get($data, '@attributes', []));
+            unset($data['@attributes']);
+
+            foreach ($data as $key => $value) {
+                if (is_numeric($key)) {
+                    $result .= self::buildXml($value, null, $attributes);
+                } else {
+                    $result .= self::buildXml($value, $key);
+                }
+            }
+
+            return self::wrapNode($parent_node, $result, $attributes);
+        }
+
+        /**
+         * Wrap $content in an xml node
+         * ie <node attrName="attrVal">content</node>
+         *
+         * @param string $name
+         * @param string $content
+         * @param string $attributes
+         *
+         * @return null|string
+         */
+        private static function wrapNode($name, $content = null, $attributes = '') {
+            if (empty($name)) {
+                return $content;
+            }
+
+            $node = "<$name";
+            if ($attributes) {
+                $node .= ' ' . trim($attributes);
+            }
+
+            if (empty($content)) {
+                return $node . '/>';
+            }
+
+            return $node . '>' . $content . "</$name>";
+        }
+
+        /**
+         * @param array $attributes
+         *
+         * @return null|string
+         */
+        private static function getAttributesString(array $attributes) {
+            if (empty($attributes)) {
+                return null;
+            }
+            $result = '';
+            foreach ($attributes as $k => $v) {
+                $result .= $k . '="' . $v . '" ';
+            }
+
+
+            return $result;
+        }
+
+        /**
          * @param SimpleXMLElement $xml
          *
          * @return array|mixed
@@ -142,77 +214,5 @@
             }
 
             return $array;
-        }
-
-        /**
-         * @param mixed       $data
-         * @param string|null $parent_node
-         * @param string      $attributes [optional]
-         *
-         * @return string
-         */
-        private static function buildXml($data, $parent_node, $attributes = null) {
-            if (!is_array($data)) {
-                return self::wrapNode($parent_node, $data, $attributes);
-            }
-
-            $result = '';
-            $attributes = self::getAttributesString(array_get($data, '@attributes', []));
-            unset($data['@attributes']);
-
-            foreach ($data as $key => $value) {
-                if (is_numeric($key)) {
-                    $result .= self::buildXml($value, null, $attributes);
-                } else {
-                    $result .= self::buildXml($value, $key);
-                }
-            }
-
-            return self::wrapNode($parent_node, $result, $attributes);
-        }
-
-        /**
-         * Wrap $content in an xml node
-         * ie <node attrName="attrVal">content</node>
-         *
-         * @param string $name
-         * @param string $content
-         * @param string $attributes
-         *
-         * @return null|string
-         */
-        private static function wrapNode($name, $content = null, $attributes = '') {
-            if (empty($name)) {
-                return $content;
-            }
-
-            $node = "<$name";
-            if ($attributes) {
-                $node .= ' ' . trim($attributes);
-            }
-
-            if (empty($content)) {
-                return $node . '/>';
-            }
-
-            return $node . '>' . $content . "</$name>";
-        }
-
-        /**
-         * @param array $attributes
-         *
-         * @return null|string
-         */
-        private static function getAttributesString(array $attributes) {
-            if (empty($attributes)) {
-                return null;
-            }
-            $result = '';
-            foreach ($attributes as $k => $v) {
-                $result .= $k . '="' . $v . '" ';
-            }
-
-
-            return $result;
         }
     }
