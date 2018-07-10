2d25cb091d40faeeb942b77576c78e86f4cebe5b
Chris <chris@landlordwebsolutions.com>
2018-07-10 11:57:40 -0400

    set display_errors = 1 before doing anything else in bootstrap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,17 +3,17 @@
     use EasyDb\Events\Listeners;
     use rAPId\Config\Config;
 
+    // Display Errors when in development environment
+    if (is_dev()) {
+        ini_set('display_errors', 1);
+    }
+
     $root = getcwd();
     require $root . '/vendor/autoload.php';
 
     // Load the .env into $_ENV
     $dotenv = new Dotenv\Dotenv($root);
     $dotenv->load();
-
-    // Display Errors when in development environment
-    if (env('ENVIRONMENT', 'development') === 'development') {
-        ini_set('display_errors', 1);
-    }
 
     Config::load('config/rAPId.php');
     Config::load('config/database.php', 'db');
