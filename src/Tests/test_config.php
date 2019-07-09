6eff0415d4839b0b44713e7bc53c3e2caff4db89
Chris <chris@landlordwebsolutions.com>
2019-07-09 14:08:45 -0400

    adjust spacing

--- src/Tests/test_config.php
+++ src/Tests/test_config.php
@@ -8,13 +8,12 @@
          * Requests without a controller specified will be routed to this controller
          *  * Must implement rAPId\Foundation\Controller
          */
-
         'default_controller' => 'rAPId\Tests\TestDefaultController',
+
 
         /**
          * Invalid requests will result in this page and a 404 response
          */
-
         'error_404_page' => 'src/404.html',
 
 
@@ -26,13 +25,14 @@
          *  - rAPId\Data\JsonSerializer
          *  - rAPId\Data\XmlSerializer
          */
-
         'output_serializer' => \rAPId\Data\Serialization\JsonSerializer::class,
+
 
         /**
          * Added for testing purposes
          */
         'test_load_success' => 'SUCCESS',
+
 
         /**
          * The class that will handle the logging for all errors/exceptions that occur
@@ -41,6 +41,7 @@
          * This class must implement the \rAPId\Debug\Logger interface
          */
         'error_logger'      => \rAPId\Debug\ServerLog::class,
+
 
         /**
          * path to the folder in which to keep the log files from the ServerLog
